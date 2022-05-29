def3651827195e9c3951fc8c675c2e98618ebde7
prado <pradodje@gmail.com>
2022-05-28 18:18:13 -0700

    Add stubs for edit and delete page onClick functions

--- src/notebook/editor/editor-header/EditorHeader.jsx
+++ src/notebook/editor/editor-header/EditorHeader.jsx
@@ -6,14 +6,32 @@ import './EditorHeader.css';
 export default function EditorHeader(props) {
     const {pages, setPages} = useContext(Pages)
     const {currentPage, setCurrentPage} = useContext(CurrentPage)
+    
+    const editPage = () => {
+        console.log('Edit Page Clicked')
+    }
+
+    const deletePage = () => {
+        console.log('Delete Page Clicked')
+    }
 
     return (
         <div id="editor-header">
             <div id="page-title">
                 <h1>{pages[currentPage].title}</h1>
             </div>
-            <button className="editor-button" id="edit-page">Edit Page</button>
-            <button className="editor-button" id="delete-page">Delete Page</button>
+            
+            <button
+                className="editor-button"
+                id="edit-page"
+                onClick={editPage}
+            >Edit Page</button>
+
+            <button
+                className="editor-button"
+                id="delete-page"
+                onClick={deletePage}
+            >Delete Page</button>
         </div>
     )
 }
