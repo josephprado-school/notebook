83ef51fa07d427b6d386cee2e15c8111a73b9d1c
prado <pradodje@gmail.com>
2022-05-28 21:59:19 -0700

    Implement edit page button
    - when user clicks on edit page button, the page title is replaced with
      a text input and edit and delete page buttons are replaced with
      confirm and cancel buttons.
    - if user clicks confirm, page title is updated to new title
    - If user clicks cancel or switches to another page in page listing,
      page title is unchanged
    - new bug: the elipses for long page titles seems to have stopped
      working in the list of pages. Need to investigate further

--- src/notebook/editor/editor-header/EditorHeader.jsx
+++ src/notebook/editor/editor-header/EditorHeader.jsx
@@ -1,18 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Pages } from '../../contexts/Pages.js';
 import { CurrentPage } from '../../contexts/CurrentPage.js';
+import EditorButton from "./editor-button/EditorButton.jsx";
 import './EditorHeader.css';
 
 export default function EditorHeader(props) {
     const {pages, setPages} = useContext(Pages)
     const {currentPage, setCurrentPage} = useContext(CurrentPage)
-
-    const pageExists = (pages.length > 0)
-    const title = pageExists ? pages[currentPage].title : ""
     
-    const editPage = () => {
-        console.log('Edit Page Clicked')
-    }
+    const pageExists = pages.length > 0
+    const title = pageExists ? pages[currentPage].title : ""
+
+    const [newTitle, setNewTitle] = useState(title)
+    const [editingTitle, setEditingTitle] = useState(false)
+
+    const visibleIf = active => active ? null : 'hidden'
 
     const deletePage = () => {
         if (pageExists) {
@@ -23,23 +25,62 @@ export default function EditorHeader(props) {
         }
     }
 
+    const comfirm = () => {
+        const newPages = [...pages]
+        newPages[currentPage].title = newTitle
+        setPages(newPages)
+        setEditingTitle(false)
+    }
+
+    const cancel = () => {
+        setEditingTitle(false)
+        setNewTitle(title)
+    }
+
+    // not sure why title needs to be in the array but excluding it was causing an error
+    // "React Hook useEffect has a missing dependency: 'title'. Either include it or remove the dependency array"
+    useEffect(cancel, [currentPage, title])
+
     return (
         <div id="editor-header">
             <div id="page-title">
-                <h1>{title}</h1>
+                <h1 className={visibleIf(!editingTitle)}>{title}</h1>
+                <input
+                    className={visibleIf(editingTitle)}
+                    type="text"
+                    id="edit-page-title"
+                    value={newTitle}
+                    onChange={e => setNewTitle(e.target.value)}
+                />
             </div>
-            
-            <button
-                className="editor-button"
-                id="edit-page"
-                onClick={editPage}
-            >Edit Page</button>
 
-            <button
-                className="editor-button"
-                id="delete-page"
-                onClick={deletePage}
-            >Delete Page</button>
+            <EditorButton
+                visibility={visibleIf(!editingTitle)}
+                name='Edit Page'
+                action={() => setEditingTitle(true)}
+                disabled={!pageExists}
+            />
+
+            <EditorButton
+                visibility={visibleIf(!editingTitle)}
+                name='Delete Page'
+                action={deletePage}
+                disabled={!pageExists}
+            />
+
+            <EditorButton
+                visibility={visibleIf(editingTitle)}
+                name='Confirm'
+                action={comfirm}
+                color='green'
+            />
+
+            <EditorButton
+                visibility={visibleIf(editingTitle)}
+                name='Cancel'
+                action={cancel}
+                color='red'
+            />
         </div>
     )
 }
---
+++ src/notebook/editor/editor-header/editor-button/EditorButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './EditorButton.css';
+
+export default function EditorButton(props) {
+    return (
+        <button
+            className={`editor-button ${props.visibility} ${props.color}`}
+            onClick={props.action}
+            disabled={props.disabled}
+        >{props.name}</button>
+    )
+}
