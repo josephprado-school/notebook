315fb8123a63d8cdaeb959fd20cab8c9e25f401d
prado <pradodje@gmail.com>
2022-05-30 03:15:32 -0700

    Save page content after 500ms of inactivity

--- src/notebook/editor/Editor.jsx
+++ src/notebook/editor/Editor.jsx
@@ -1,21 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState} from "react";
 import { Pages } from "../contexts/Pages";
 import { CurrentPage } from "../contexts/CurrentPage";
+import { StorageManager } from "../contexts/StorageManager";
 import Header from "./header/Header";
 import "./Editor.css";
 
 export default function Editor(props) {
     const {pages, setPages} = useContext(Pages)
     const {currentPage} = useContext(CurrentPage)
+    const {storePage} = useContext(StorageManager)
 
     const pageExists = (pages.length > 0)
     const content = pageExists ? pages[currentPage].content : ""
+    
+    const [debouncer, setDebouncer] = useState()
+    
+    const startDebouncer = () => {
+        clearTimeout(debouncer)
+
+        setDebouncer( 
+            setTimeout(() => {
+                storePage(pages[currentPage], currentPage)
+            }, 500)
+        )
+    }
 
     const textConentChange = (newContent) => {
         if (pageExists) {
             const newPages = [...pages]
             newPages[currentPage].content = newContent
             setPages(newPages)
+
+            startDebouncer()
         }
     }
 
