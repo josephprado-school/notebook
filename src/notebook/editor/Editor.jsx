9e9cb6dd6745842a62ba59808c8af3e4278de6d3
prado <pradodje@gmail.com>
2022-05-30 03:27:28 -0700

    Remove unused import

--- src/notebook/editor/Editor.jsx
+++ src/notebook/editor/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState} from "react";
+import React, { useContext, useState} from "react";
 import { Pages } from "../contexts/Pages";
 import { CurrentPage } from "../contexts/CurrentPage";
 import { StorageManager } from "../contexts/StorageManager";
